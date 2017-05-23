9f96eb6e1b70d2b8dfeee0d3171c0ff639585612
jonathan <jonathanecm@icloud.com>
2017-05-23 10:17:41 -0500

    Seting up D3

--- submissions/camacho_jonathan/A4/runfile.R
+++ submissions/camacho_jonathan/A4/runfile.R
@@ -1,5 +1,5 @@
 # Cleans existing files and directories and creates new directores.
-paths <- c("./data", "./graphics")
+paths <- c("./graphics")
 
 for (path in paths) {
     unlink(path, recursive = TRUE) 
@@ -8,5 +8,6 @@ for (path in paths) {
 
 source("00_download_data.R")
 source("01_clean_data.R")
+
 
 
